993b7231a5c7ab184effe79cad7761da523da628
wildeslam <3160104749@zju.edu.cn>
2023-06-06 10:49:02 +0800

    feat:count record cases & list them

--- arex-storage-model/src/main/java/com/arextest/model/replay/OrderCondition.java
+++ arex-storage-model/src/main/java/com/arextest/model/replay/OrderCondition.java
@@ -2,8 +2,10 @@ package com.arextest.model.replay;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class OrderCondition {
     /**
