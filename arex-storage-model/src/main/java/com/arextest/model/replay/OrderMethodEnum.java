92bcdbd9ecb44102897b72401020d6d1b5d17794
wildeslam <3160104749@zju.edu.cn>
2023-06-06 11:05:49 +0800

    feat:count record cases & list them

--- arex-storage-model/src/main/java/com/arextest/model/replay/OrderMethodEnum.java
+++ arex-storage-model/src/main/java/com/arextest/model/replay/OrderMethodEnum.java
@@ -1,13 +1,15 @@
 package com.arextest.model.replay;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public enum OrderMethodEnum {
     ASCENDING(1),
     DESCENDING(2);
 
     private final int code;
+
+    public int getCode() {
+        return code;
+    }
 }
