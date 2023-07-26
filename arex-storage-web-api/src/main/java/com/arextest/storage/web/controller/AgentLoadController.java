9e32a49ff66e6934d95c8ffee2057a095d27d6f4
zhitaogan <owengan@bu.edu>
2023-07-26 14:41:44 +0800

    feat: "adding a transparent transmission interface of a new interface in the storage service"

--- arex-storage-web-api/src/main/java/com/arextest/storage/web/controller/AgentLoadController.java
+++ arex-storage-web-api/src/main/java/com/arextest/storage/web/controller/AgentLoadController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
