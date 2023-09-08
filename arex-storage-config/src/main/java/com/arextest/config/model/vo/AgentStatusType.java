d7aed978014ab532fff1df17fa24afd447d21352
coryhh <jschenrui@163.com>
2023-09-08 18:46:46 +0800

    refactor: refactor config

---
+++ arex-storage-config/src/main/java/com/arextest/config/model/vo/AgentStatusType.java
@@ -0,0 +1,10 @@
+package com.arextest.config.model.vo;
+
+public class AgentStatusType {
+    public static final String NONE = "NONE";
+    public static final String START = "START";
+    public static final String UN_START = "UN_START";
+    public static final String WORKING = "WORKING";
+    public static final String SLEEPING = "SLEEPING";
+    public static final String SHUTDOWN = "SHUTDOWN";
+}
