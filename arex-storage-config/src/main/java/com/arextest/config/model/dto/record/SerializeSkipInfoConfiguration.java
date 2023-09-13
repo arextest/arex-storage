dd9bd566846cbce332a395f4b8c193ec79c767a6
zhitaogan <owengan@bu.edu>
2023-09-13 17:02:40 +0800

    feat: "add @Data"

--- arex-storage-config/src/main/java/com/arextest/config/model/dto/record/SerializeSkipInfoConfiguration.java
+++ arex-storage-config/src/main/java/com/arextest/config/model/dto/record/SerializeSkipInfoConfiguration.java
@@ -1,5 +1,10 @@
 package com.arextest.config.model.dto.record;
 
+import lombok.Data;
+
+import javax.annotation.sql.DataSourceDefinition;
+
+@Data
 public class SerializeSkipInfoConfiguration {
     private String fullClassName;
     private String fieldName;
