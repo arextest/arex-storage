b9ec296f8507396decbf3a1fe53d3fe7d0ab8a03
qzmo <qzmo@trip.com>
2023-10-12 10:57:30 +0800

    feat: async handler

--- arex-storage-web-api/src/main/java/com/arextest/storage/service/mockerhandlers/CoverageMockerHandler.java
+++ arex-storage-web-api/src/main/java/com/arextest/storage/service/mockerhandlers/CoverageMockerHandler.java
@@ -12,8 +12,10 @@ import com.arextest.storage.service.MockSourceEditionService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import javax.annotation.Resource;
+import java.util.concurrent.*;
 
 @Component
 @Slf4j
@@ -26,6 +28,8 @@ public class CoverageMockerHandler implements MockerSaveHandler<AREXMocker> {
     private CoverageRepository coverageRepository;
     @Resource
     private CacheProvider cacheProvider;
+    @Resource
+    private ThreadPoolExecutor coverageHandlerExecutor;
 
     @Override
     public MockCategoryType getMockCategoryType() {
@@ -41,41 +45,10 @@ public class CoverageMockerHandler implements MockerSaveHandler<AREXMocker> {
      */
     @Override
     public void handle(AREXMocker coverageMocker) {
-        if (StringUtils.isEmpty(coverageMocker.getOperationName()) || coverageMocker.getOperationName().equals("0")) {
-            LOGGER.warn("CoverageMockerHandler handle error, operationName is empty, recordId:{}", coverageMocker.getRecordId());
-            if (!StringUtils.isEmpty(coverageMocker.getRecordId())) {
-                // getting operationName(Coverage key) as 0 but having recordId, meaning this is an extremely simple and meaningless case, remove it
-                mockSourceEditionService.removeByRecordId(ProviderNames.DEFAULT, coverageMocker.getRecordId());
-            }
-            return;
-        }
-        try {
-            final RepositoryProvider<Mocker> pinedProvider = repositoryProviderFactory.findProvider(ProviderNames.AUTO_PINNED);
-            assert pinedProvider != null;
-
-            String incomingCaseId = coverageMocker.getRecordId();
-            Mocker pinned = pinedProvider.findEntryFromAllType(incomingCaseId);
-            // Mocker rolling = rollingProvider.findEntryFromAllType(newCaseId);
-
-            if (pinned != null) {
-                coverageRepository.updatePathByRecordId(incomingCaseId, coverageMocker);
-                LOGGER.info("CoverageMockerHandler handle update, recordId:{}, pathKey: {}", incomingCaseId, coverageMocker.getOperationName());
-            } else {
-                boolean locked = cacheProvider.putIfAbsent((coverageMocker.getAppId() + coverageMocker.getOperationName()).getBytes(),
-                        60 * 24 * 12L,
-                        coverageMocker.getRecordId().getBytes());
-
-                if (locked) {
-                    transferEntry(coverageMocker, incomingCaseId);
-                    LOGGER.info("CoverageMockerHandler handle transfer, recordId:{}, pathKey: {}", incomingCaseId, coverageMocker.getOperationName());
-                } else {
-                    mockSourceEditionService.removeByRecordId(ProviderNames.DEFAULT, incomingCaseId);
-                    LOGGER.info("CoverageMockerHandler handle remove, recordId:{}, pathKey: {}", incomingCaseId, coverageMocker.getOperationName());
-                }
-            }
-        } catch (Exception e) {
-            LOGGER.error("CoverageMockerHandler handle error", e);
-        }
+        CoverageTask task = new CoverageTask(coverageMocker);
+        coverageHandlerExecutor.submit(task);
+        LOGGER.info("CoverageMockerHandler handle submit, recordId:{}, pathKey: {}, pool queue size: {}",
+                coverageMocker.getRecordId(), coverageMocker.getOperationName(), coverageHandlerExecutor.getQueue().size());
     }
 
     private void transferEntry(AREXMocker coverageMocker, String incomingCaseId) {
@@ -95,4 +68,50 @@ public class CoverageMockerHandler implements MockerSaveHandler<AREXMocker> {
             LOGGER.error("move entry to auto pinned failed, caseId:{}", incomingCaseId);
         }
     }
+
+    private class CoverageTask implements Runnable {
+        private final AREXMocker coverageMocker;
+        CoverageTask(AREXMocker coverageMocker) {
+            this.coverageMocker = coverageMocker;
+        }
+        @Override
+        public void run() {
+            if (StringUtils.isEmpty(coverageMocker.getOperationName()) || coverageMocker.getOperationName().equals("0")) {
+                LOGGER.warn("CoverageMockerHandler handle error, operationName is empty, recordId:{}", coverageMocker.getRecordId());
+                if (!StringUtils.isEmpty(coverageMocker.getRecordId())) {
+                    // getting operationName(Coverage key) as 0 but having recordId, meaning this is an extremely simple and meaningless case, remove it
+                    mockSourceEditionService.removeByRecordId(ProviderNames.DEFAULT, coverageMocker.getRecordId());
+                }
+                return;
+            }
+            try {
+                final RepositoryProvider<Mocker> pinedProvider = repositoryProviderFactory.findProvider(ProviderNames.AUTO_PINNED);
+                assert pinedProvider != null;
+
+                String incomingCaseId = coverageMocker.getRecordId();
+                Mocker pinned = pinedProvider.findEntryFromAllType(incomingCaseId);
+                // Mocker rolling = rollingProvider.findEntryFromAllType(newCaseId);
+
+                if (pinned != null) {
+                    coverageRepository.updatePathByRecordId(incomingCaseId, coverageMocker);
+                    LOGGER.info("CoverageMockerHandler handle update, recordId:{}, pathKey: {}", incomingCaseId, coverageMocker.getOperationName());
+                } else {
+                    boolean locked = cacheProvider.putIfAbsent((coverageMocker.getAppId() + coverageMocker.getOperationName()).getBytes(),
+                            60 * 24 * 12L,
+                            coverageMocker.getRecordId().getBytes());
+
+                    if (locked) {
+                        transferEntry(coverageMocker, incomingCaseId);
+                        LOGGER.info("CoverageMockerHandler handle transfer, recordId:{}, pathKey: {}", incomingCaseId, coverageMocker.getOperationName());
+                    } else {
+                        mockSourceEditionService.removeByRecordId(ProviderNames.DEFAULT, incomingCaseId);
+                        LOGGER.info("CoverageMockerHandler handle remove, recordId:{}, pathKey: {}", incomingCaseId, coverageMocker.getOperationName());
+                    }
+                }
+            } catch (Exception e) {
+                LOGGER.error("CoverageMockerHandler handle error", e);
+            }
+        }
+    }
+
 }
