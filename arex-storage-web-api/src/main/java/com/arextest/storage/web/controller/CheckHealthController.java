53867be9bd116ab5964c19f04a72b544f5e367c4
王雨疏 <35320326+wildeslam@users.noreply.github.com>
2023-11-17 01:58:18 -0600

    Feat/pom version (#114)
    * feat: get pom version at vi/health interface
    
    * feat: get pom version at vi/health interface
    
    * feat: get pom version at vi/health interface
    
    ---------
    
    Co-authored-by: yushuwang <yushuwang@trip.com>

--- arex-storage-web-api/src/main/java/com/arextest/storage/web/controller/CheckHealthController.java
+++ arex-storage-web-api/src/main/java/com/arextest/storage/web/controller/CheckHealthController.java
@@ -2,10 +2,8 @@ package com.arextest.storage.web.controller;
 
 import com.arextest.common.model.response.Response;
 import com.arextest.common.utils.ResponseUtils;
-import java.io.FileReader;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,27 +15,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/vi/")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class CheckHealthController {
-  private static Model POM_MODEL;
-
-  static {
-    try {
-      POM_MODEL = new MavenXpp3Reader().read(new FileReader("pom.xml"));
-    } catch (Exception e) {
-      LOGGER.error("Read pom failed!", e);
-    }
-  }
+  @Value("${pom.version}")
+  private String VERSION;
 
   @GetMapping(value = "/health", produces = "application/json")
   @ResponseBody
   public Response checkHealth() {
-    return ResponseUtils.successResponse(getVersion());
+    return ResponseUtils.successResponse(VERSION);
   }
 
-  private static String getVersion() {
-    if (POM_MODEL != null) {
-      return POM_MODEL.getVersion();
-    } else {
-      return "error";
-    }
-  }
 }
