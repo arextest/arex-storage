b6f33daab1ab1b4080ff466eaf914470b4d5379d
Nathan Mo <54135657+QizhengMo@users.noreply.github.com>
2024-01-17 20:44:54 +0800

    Feat/multi env config (#152)
    * fix: nested obj

--- arex-storage-config/src/main/java/com/arextest/config/model/dto/record/MultiEnvConfig.java
+++ arex-storage-config/src/main/java/com/arextest/config/model/dto/record/MultiEnvConfig.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 public interface MultiEnvConfig<T> {
-  List<T> getMultiEnvConfigs();
-  void setMultiEnvConfigs(List<T> multiEnvConfigs);
+  List<MultiEnvConfig<T>> getMultiEnvConfigs();
+  void setMultiEnvConfigs(List<MultiEnvConfig<T>> multiEnvConfigs);
 
   Map<String, String> getEnvTags();
   void setEnvTags(Map<String, String> envTags);
