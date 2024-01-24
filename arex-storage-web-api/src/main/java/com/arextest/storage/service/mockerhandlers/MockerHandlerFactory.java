ecc6eedcf2d46b8cb6bf515a612f30feccf826fc
qzmo <qzmo@trip.com>
2024-01-24 13:38:58 +0800

    feat: init handler map on init

--- arex-storage-web-api/src/main/java/com/arextest/storage/service/mockerhandlers/MockerHandlerFactory.java
+++ arex-storage-web-api/src/main/java/com/arextest/storage/service/mockerhandlers/MockerHandlerFactory.java
@@ -6,12 +6,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MockerHandlerFactory<T extends Mocker> {
   private final Map<MockCategoryType, List<MockerSaveHandler<T>>> categoryHandlers;
-  public MockerHandlerFactory(List<MockerSaveHandler<T>> handlers) {
+  public MockerHandlerFactory(@Autowired List<MockerSaveHandler<T>> handlers) {
     this.categoryHandlers = handlers
         .stream()
         .collect(Collectors.groupingBy(MockerSaveHandler::getMockCategoryType));
