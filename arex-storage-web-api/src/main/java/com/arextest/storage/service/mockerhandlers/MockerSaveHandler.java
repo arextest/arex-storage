5f23cfdf977538f10a33b5d1ab6c00dac30cd268
Nathan Mo <54135657+QizhengMo@users.noreply.github.com>
2024-01-24 18:23:21 +0800

    Feat/mocker handler event (#160)
    * chore: remove generic

--- arex-storage-web-api/src/main/java/com/arextest/storage/service/mockerhandlers/MockerSaveHandler.java
+++ arex-storage-web-api/src/main/java/com/arextest/storage/service/mockerhandlers/MockerSaveHandler.java
@@ -3,9 +3,9 @@ package com.arextest.storage.service.mockerhandlers;
 import com.arextest.model.mock.MockCategoryType;
 import com.arextest.model.mock.Mocker;
 
-public interface MockerSaveHandler<T extends Mocker> {
+public interface MockerSaveHandler {
 
   MockCategoryType getMockCategoryType();
 
-  void handle(T item);
+  void handle(Mocker item);
 }
