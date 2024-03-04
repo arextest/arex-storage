9d74403db166c3ffd2e6ff94d2cd132965686443
Nathan Mo <54135657+QizhengMo@users.noreply.github.com>
2024-03-04 15:32:39 +0800

    feat: test framework init (#177)
    * feat: test framework init

---
+++ arex-storage-web-api/src/test/java/com/arextest/storage/web/controller/AgentRecordingControllerTest.java
@@ -0,0 +1,27 @@
+package com.arextest.storage.web.controller;
+
+import com.arextest.model.mock.AREXMocker;
+import com.arextest.storage.metric.AgentWorkingMetricService;
+import com.arextest.storage.service.AgentWorkingService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AgentRecordingControllerTest {
+  @InjectMocks
+  AgentRecordingController service;
+
+  @Mock
+  private AgentWorkingService agentWorkingService;
+  @Mock
+  private AgentWorkingMetricService agentWorkingMetricService;
+
+
+  @Test
+  public void test() {
+    service.save(new AREXMocker());
+  }
+}
