a6657c4ebd375332607f3b1cae49f21655a24330
rchen9 <rchen9@trip.com>
2024-06-12 15:19:18 +0800

    feat: support the config of "spring-redis"

--- arex-storage-web-api/src/main/java/com/arextest/storage/beans/RedisAutoConfiguration.java
+++ arex-storage-web-api/src/main/java/com/arextest/storage/beans/RedisAutoConfiguration.java
@@ -3,12 +3,18 @@ package com.arextest.storage.beans;
 import com.arextest.common.cache.CacheProvider;
 import com.arextest.common.cache.DefaultRedisCacheProvider;
 import com.arextest.common.cache.SentinelRedisCacheProvider;
+import com.arextest.common.cache.redistemplate.RedisTemplateCacheProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
  * @author wildeslam.
@@ -16,25 +22,50 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration(proxyBeanMethods = false)
-@EnableConfigurationProperties({StorageConfigurationProperties.class})
 @Slf4j
 public class RedisAutoConfiguration {
 
-  private final StorageConfigurationProperties properties;
+  @Value("${arex.redis.uri:}")
+  private String redisUri;
 
-  public RedisAutoConfiguration(StorageConfigurationProperties configurationProperties) {
-    properties = configurationProperties;
+  @Value("${arex.redis.sentinelUrl:}")
+  private String sentinelUrl;
+
+
+  @Bean
+  @ConditionalOnMissingBean(CacheProvider.class)
+  @ConditionalOnExpression("!'${arex.redis.uri:}'.isEmpty() or !'${arex.redis.sentinelUrl:}'.isEmpty()")
+  public CacheProvider cacheProvider() {
+    if (StringUtils.isNotEmpty(sentinelUrl)) {
+      return new SentinelRedisCacheProvider(sentinelUrl);
+    }
+    return new DefaultRedisCacheProvider(redisUri);
+  }
+
+  @Bean
+  @ConditionalOnExpression("!'${arex.redis.uri:}'.isEmpty() or !'${arex.redis.sentinelUrl:}'.isEmpty()")
+  public RedissonClient redissonClient(CacheProvider cacheProvider) {
+    return cacheProvider.getRedissionClient();
+  }
+
+  @Bean
+  @ConditionalOnExpression("'${arex.redis.uri:}'.isEmpty() and '${arex.redis.sentinelUrl:}'.isEmpty()")
+  public RedisTemplate<byte[], byte[]> redisTemplate(
+      RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+    template.setConnectionFactory(redisConnectionFactory);
+    template.setKeySerializer(RedisSerializer.byteArray());
+    template.setValueSerializer(RedisSerializer.byteArray());
+    return template;
   }
 
   @Bean
   @ConditionalOnMissingBean(CacheProvider.class)
-  public CacheProvider cacheProvider() {
-    if (StringUtils.isNotEmpty(properties.getCache().getSentinelUrl())) {
-      return new SentinelRedisCacheProvider(properties.getCache().getSentinelUrl());
-    }
-    if (StringUtils.isEmpty(properties.getCache().getUri())) {
-      return new DefaultRedisCacheProvider();
-    }
-    return new DefaultRedisCacheProvider(properties.getCache().getUri());
+  @ConditionalOnExpression("'${arex.redis.uri:}'.isEmpty() and '${arex.redis.sentinelUrl:}'.isEmpty()")
+  public CacheProvider redisCacheProvider(RedisTemplate redisTemplate,
+      RedissonClient redissonClient) {
+    return new RedisTemplateCacheProvider(redisTemplate, redissonClient);
   }
+
+
 }
