12c7059e4d237c59b50aaa355d676e57c280223e
wildeslam <yushuwang@trip.com>
2024-06-27 19:30:43 +0800

    feat: add exclusion configs for load interface

--- arex-storage-web-api/src/main/java/com/arextest/storage/service/QueryConfigService.java
+++ arex-storage-web-api/src/main/java/com/arextest/storage/service/QueryConfigService.java
@@ -3,16 +3,19 @@ package com.arextest.storage.service;
 import static com.arextest.diff.utils.JacksonHelperUtil.objectMapper;
 import com.arextest.common.cache.CacheProvider;
 import com.arextest.config.model.dto.ComparisonExclusionsConfiguration;
+import com.arextest.config.model.dto.application.ApplicationOperationConfiguration;
 import com.arextest.config.model.vo.ConfigComparisonExclusionsVO;
 import com.arextest.config.model.vo.QueryConfigOfCategoryRequest;
 import com.arextest.config.model.vo.QueryConfigOfCategoryResponse;
 import com.arextest.config.model.vo.QueryConfigOfCategoryResponse.QueryConfigOfCategory;
+import com.arextest.config.repository.impl.ApplicationOperationConfigurationRepositoryImpl;
 import com.arextest.config.repository.impl.ComparisonExclusionsConfigurationRepositoryImpl;
 import com.arextest.model.mock.Mocker;
 import com.arextest.storage.cache.CacheKeyUtils;
 import com.arextest.storage.client.HttpWebServiceApiClient;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -44,6 +47,9 @@ public class QueryConfigService {
   @Value("${arex.query.systemConfig.url}")
   private String querySystemConfigUrl;
 
+  @Value("${arex.query.contracts.url}")
+  private String queryContractsUrl;
+
   @Value("${arex.config.cache.expired.seconds:600}")
   private long cacheExpiredSeconds;
 
@@ -55,6 +61,9 @@ public class QueryConfigService {
 
   @Resource
   private ComparisonExclusionsConfigurationRepositoryImpl comparisonExclusionsConfigurationRepository;
+
+  @Resource
+  private ApplicationOperationConfigurationRepositoryImpl applicationOperationConfigurationRepository;
 
   public QueryConfigOfCategory queryConfigOfCategory(Mocker mocker) {
     if (mocker.getCategoryType().isSkipComparison()) {
@@ -95,7 +104,7 @@ public class QueryConfigService {
     List<ComparisonExclusionsConfiguration> configs = comparisonExclusionsConfigurationRepository.listBy(
         appId, COMPARE_CONFIG_TYPE);
     if (CollectionUtils.isNotEmpty(configs)) {
-      return getComparisonExclusions(configs);
+      return getComparisonExclusions(configs, appId);
     }
     return Collections.emptyList();
   }
@@ -110,11 +119,61 @@ public class QueryConfigService {
   }
 
   private List<ConfigComparisonExclusionsVO> getComparisonExclusions(
-      List<ComparisonExclusionsConfiguration> configs) {
+      List<ComparisonExclusionsConfiguration> configs, String appId) {
 
-    Map<String, List<ComparisonExclusionsConfiguration>> groupByOperationNameAndType = configs.stream()
-        .collect(
-            Collectors.groupingBy(config -> config.getOperationName() + config.getOperationType()));
+    List<ApplicationOperationConfiguration> operationConfigurationList =
+        applicationOperationConfigurationRepository.listBy(appId);
+    Map<String, ApplicationOperationConfiguration> operationConfigurationMap = operationConfigurationList.stream()
+        .collect(Collectors.toMap(ApplicationOperationConfiguration::getId, operation -> operation));
+
+    QueryContractRequestType requestType = new QueryContractRequestType();
+    requestType.setAppId(appId);
+    QueryContractResponse response = httpWebServiceApiClient.jsonPost(queryContractsUrl, requestType, QueryContractResponse.class);
+    Map<String, AppContractDto> contractMap = new HashMap<>();
+    if (response != null && response.getBody() != null) {
+      contractMap.putAll(response.getBody().stream()
+          .collect(Collectors.toMap(AppContractDto::getId, contract -> contract)));
+    }
+
+
+    List<ComparisonExclusionsConfiguration> newConfigs = new ArrayList<>();
+    for (ComparisonExclusionsConfiguration config : configs) {
+      //dependency
+      if (config.getDependencyId() != null) {
+        AppContractDto contract = contractMap.get(config.getDependencyId());
+        if (contract != null) {
+          config.setOperationType(contract.getOperationType());
+          config.setOperationName(contract.getOperationName());
+          newConfigs.add(config);
+        }
+        continue;
+      }
+
+      ApplicationOperationConfiguration operationConfiguration = operationConfigurationMap.get(config.getOperationId());
+      if (operationConfiguration != null) {
+        config.setOperationName(operationConfiguration.getOperationName());
+        if (operationConfiguration.getOperationTypes().size() > 1) {
+          for (String operationType : operationConfiguration.getOperationTypes()) {
+            ComparisonExclusionsConfiguration newConfig = new ComparisonExclusionsConfiguration();
+            newConfig.setOperationName(config.getOperationName());
+            newConfig.setOperationType(operationType);
+            newConfig.setExclusions(config.getExclusions());
+            newConfigs.add(newConfig);
+          }
+        } else if (operationConfiguration.getOperationTypes().size() == 1) {
+          String operationType = operationConfiguration.getOperationTypes().iterator().next();
+          config.setOperationType(operationType);
+          newConfigs.add(config);
+        }
+      } else {
+        newConfigs.add(config);
+      }
+    }
+
+    Map<String, List<ComparisonExclusionsConfiguration>> groupByOperationNameAndType = newConfigs
+        .stream()
+        .collect(Collectors.groupingBy(
+            config -> config.getOperationName() + config.getOperationType()));
     List<ConfigComparisonExclusionsVO> result = new ArrayList<>();
     for (Entry<String, List<ComparisonExclusionsConfiguration>> entry : groupByOperationNameAndType
         .entrySet()) {
@@ -188,5 +247,40 @@ public class QueryConfigService {
     private Set<String> ignoreNodeSet;
   }
 
+  @Data
+  public static class QueryContractRequestType {
+
+    // query global contract
+    private String appId;
+    // query entryPointContract
+    private String operationId;
+    // query dependency
+    private String operationType;
+    private String operationName;
+
+  }
+
+  @Data
+  public static class QueryContractResponse {
+
+    private List<AppContractDto> body;
+
+  }
+
+  @Data
+  public static class AppContractDto  {
+
+    private String id;
+    private String appId;
+    private Integer contractType;
+    private String operationId;
+
+    /**
+     * the operationName and operationType exist, when contractType is 2
+     */
+    private String operationName;
+    private String operationType;
+    private String contract;
+  }
 
 }
